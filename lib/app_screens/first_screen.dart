8867ea315aa4bc5717df80c0b1be2a886b47d87d
lawliet022 <deepak26022@gmail.com>
2020-04-14 19:18:24 +0530

    writing function and adding logic to code

--- lib/app_screens/first_screen.dart
+++ lib/app_screens/first_screen.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 class FirstScreen extends StatelessWidget{
@@ -7,12 +8,18 @@ class FirstScreen extends StatelessWidget{
         color: Colors.lightBlueAccent,
         child: Center(
           child: Text(
-            "Hello Flutter", 
+            getString(),
             textDirection: TextDirection.ltr,
-            style: TextStyle(color: Colors.white, fontSize: 40.0 ),
+            style: TextStyle(color: Colors.white, fontSize: 30.0 ),
             )
           ),
         );
+  }
+
+  String getString(){
+    var rand = Random();
+    int number = rand.nextInt(10);
+    return "Your lucky number is $number";
   }
 
 }
