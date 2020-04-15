a7ef9be03fbd0ed908670256bfed90b21e5b0ee3
lawliet022 <deepak26022@gmail.com>
2020-04-15 16:42:40 +0530

    Row and column in flutter

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -7,24 +7,79 @@ class Home extends StatelessWidget{
   Widget build(BuildContext context) {
     return Center(
       child: Container(
+        padding: EdgeInsets.only(left: 10.0, top:40.0),
         alignment: Alignment.center,
         color: Colors.deepPurple,
-        // width: 400.0,
-        // height: 150.0,
-        // margin: EdgeInsets.all(20.0),   // puts margin with other widgets
-        // padding: EdgeInsets.all(100.0),  // puts padding to its children 
-        child: Text(
-            "Hello there!", 
-            textDirection: TextDirection.ltr,
-            style: TextStyle(
-              decoration: TextDecoration.none,
-              fontSize: 35.0,
-              fontFamily: 'Raleway',
-              fontWeight: FontWeight.w300,
-              color: Colors.white,
-              // fontStyle: FontStyle.italic,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: Text(
+                    "Spice Jet", 
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                      decoration: TextDecoration.none,
+                      fontSize: 35.0,
+                      fontFamily: 'Raleway',
+                      fontWeight: FontWeight.w700,
+                      color: Colors.white,
+                      // fontStyle: FontStyle.italic,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Text(
+                    "From Delhi to Banglore via Bhopal", 
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                      decoration: TextDecoration.none,
+                      fontSize: 20.0,
+                      fontFamily: 'Raleway',
+                      fontWeight: FontWeight.w300,
+                      color: Colors.white,
+                      // fontStyle: FontStyle.italic,
+                    ),
+                  )
+                ),
+              ],
             ),
-          ),
+            Row(
+            children: <Widget>[
+              
+              Expanded(
+                child: Text(
+                  "Indigo", 
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    decoration: TextDecoration.none,
+                    fontSize: 35.0,
+                    fontFamily: 'Raleway',
+                    fontWeight: FontWeight.w700,
+                    color: Colors.white,
+                    // fontStyle: FontStyle.italic,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Text(
+                  "From Kolkata to Goa via Delhi", 
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    decoration: TextDecoration.none,
+                    fontSize: 20.0,
+                    fontFamily: 'Raleway',
+                    fontWeight: FontWeight.w300,
+                    color: Colors.white,
+                    // fontStyle: FontStyle.italic,
+                  ),
+                )
+              ),
+            ],
+          )
+          ], 
+        )
       ),
     );
     
